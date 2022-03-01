b8f2c73668396d37e2ee23704b5af01883f80bd1
wakeupzaman909 <hadiuzzaman909@outlook.com>
2022-03-01 22:09:59 +0600

    displaying search result in website

--- js/phone.js
+++ js/phone.js
@@ -24,9 +24,45 @@ const searchPhone=async ()=>{
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res=>res.json())
-        .then(data=>console.log(data.data))
+        .then(data=>displaySearchResult(data.data))
         .catch(error=>displayError(error));        
     }
 
 }
+const displayError=error=>{
+    document.getElementById('error-message').style.display='block';
 
+}
+
+const displaySearchResult=phones=>{
+    const searchResult=document.getElementById('search-result');
+    searchResult.textContent='';
+    if(phones.length==0){
+        const err2=document.getElementById('error2');
+        const div=document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`
+        <h2 class="text-center text-danger">No result Found!...<h2>
+        `;
+        err2.appendChild(div);
+}
+    else{
+        phones.forEach(phone=>{
+            console.log(phone);
+            const div=document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML=`
+            <div  class="card h-100 p-5 bg-light p-2 text-dark bg-opacity-50">
+                <img src="${phone.image}" class="card-img-top rounded" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                    <button onclick="loadPhoneDetail('${phone.slug}')">Details</button>
+                </div>
+            </div>
+            `;
+            searchResult.appendChild(div);
+        })
+    }
+
+};
